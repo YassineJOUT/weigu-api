3cce9662b369eb912311e1e35f92cf2e264f67d2
Yassine JOUT <yassinejout@gmail.com>
2020-06-08 16:09:45 +0100

    refactor(housekeeping):

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 /*
-  * @file-description : Authentication service provide user validation and JWT token generation
-  * @author{Yassine JOUT} yassinejout@gmail.com
-*/
+ * @file-description : Authentication service provide user validation and JWT token generation
+ * @author{Yassine JOUT} yassinejout@gmail.com
+ */
 
 // Import the required modules
 import * as bcryptjs from 'bcryptjs';
@@ -10,57 +10,50 @@ import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { USER_EXISTS } from 'src/utilities/constants';
 
-
 @Injectable()
 export class AuthService {
   constructor(
     private readonly usersService: UsersService,
-    private readonly jwtService: JwtService
-    ) {}
+    private readonly jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, pass: string): Promise<any> {
     const user = await this.usersService.findUser(email);
-   
+
     if (!user) {
       return {
         success: false,
-        error: USER_EXISTS
-        
-      }
+        error: USER_EXISTS,
+      };
     }
 
-    
     const valid = await bcryptjs.compare(pass, user.password);
-  
+
     if (!valid) {
       return {
         success: false,
-        error:  USER_EXISTS
-        
-      }
+        error: USER_EXISTS,
+      };
     }
-  
-    
+
     return {
       data: user,
       success: true,
     };
-    
   }
 
   async login(user) {
-    if(user.success){
+    if (user.success) {
       const playload = { email: user.data.email, sub: user.data._id };
       const userD = user.data;
       return {
         success: true,
-        data:{
-        access_token: this.jwtService.sign(playload),
-        user: userD
-      }
+        data: {
+          access_token: this.jwtService.sign(playload),
+          user: userD,
+        },
       };
     }
     return user;
-    
   }
 }
