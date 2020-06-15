b644f3f5d38c11fdd40420da4a8038c6826d13aa
Yassine JOUT <yassinejout@gmail.com>
2020-06-15 10:45:42 +0100

    refactor(MAIN)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-
+console.log(process.env.FRONT_HOST);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const options = {credentials: true, origin: "http://localhost:3000"};
+  const options = {credentials: true, origin: process.env.FRONT_HOST};
   app.enableCors(options);
   app.use(cookieParser());
   await app.listen(3010);
